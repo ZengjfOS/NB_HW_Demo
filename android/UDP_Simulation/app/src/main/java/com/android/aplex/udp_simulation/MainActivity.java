cf67b5782a5908935dfa71dce466a6408bcb615d
ZoroZeng <aplexos.@163.com>
2017-09-25 15:15:47 +0800

    support real time update.

--- android/UDP_Simulation/app/src/main/java/com/android/aplex/udp_simulation/MainActivity.java
+++ android/UDP_Simulation/app/src/main/java/com/android/aplex/udp_simulation/MainActivity.java
@@ -1,9 +1,12 @@
 package com.android.aplex.udp_simulation;
 
+import android.app.Activity;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +14,9 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -22,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     public EditText udp_ip;
     public EditText upd_port;
     private int current_temp = 0;
+    public Activity activity = this;
+    public String sendback;
+    public int temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // 设置三格代表温度1度
         tempControl.setAngleRate(3);
         tempControl.setTemp(0, 100, 50);
+        tempControl.setTemp(50);
 
         tempControl.setOnTempChangeListener(new TempControlView.OnTempChangeListener() {
             @Override
@@ -52,11 +62,27 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             socket = new DatagramSocket();
                             address = InetAddress.getByName(udp_ip.getText().toString());
+                            Log.e("MainActivity", udp_ip.getText().toString());
 
                             String send_str = "{\"temperature\":" + current_temp + "}";
 
                             DatagramPacket packet = new DatagramPacket(send_str.getBytes(), send_str.getBytes().length, address, Integer.valueOf(upd_port.getText().toString()));
                             socket.send(packet);
+
+                            byte[] message = new byte[128];
+                            DatagramPacket p = new DatagramPacket(message, message.length);
+                            socket.receive(p);
+                            sendback = new String(message, 0, p.getLength());
+
+                            /*
+                            activity.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast.makeText(activity, sendback, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            */
+
+                            Log.d("Udp tutorial","message:" + sendback);
 
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -65,5 +91,59 @@ public class MainActivity extends AppCompatActivity {
                 }).start();
             }
         });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DatagramSocket socket;
+                InetAddress address;
+
+                while(true) {
+                    try {
+                        socket = new DatagramSocket();
+                        address = InetAddress.getByName(udp_ip.getText().toString());
+
+                        String send_str = "refresh";
+
+                        DatagramPacket packet = new DatagramPacket(send_str.getBytes(), send_str.getBytes().length, address, Integer.valueOf(upd_port.getText().toString()));
+                        socket.send(packet);
+
+                        byte[] message = new byte[128];
+                        DatagramPacket p = new DatagramPacket(message, message.length);
+                        socket.receive(p);
+                        sendback = new String(message, 0, p.getLength());
+                        Log.d("MainActivity", sendback);
+
+                        if (sendback.length() > 0) {
+                            InputStream json = new ByteArrayInputStream(sendback.getBytes());
+                            JsonReader reader = new JsonReader(new InputStreamReader(json));
+
+                            try {
+                                reader.beginObject();
+                                while (reader.hasNext()) {
+                                    String keyName = reader.nextName();
+                                    if ("temperature".equals(keyName)) {
+                                        temperature = reader.nextInt();
+                                        activity.runOnUiThread(new Runnable() {
+                                            public void run() {
+                                                tempControl.setTemp(temperature);
+                                            }
+                                        });
+                                    }
+                                }
+                                reader.endObject();
+                            } finally {
+                                reader.close();
+                            }
+
+                        }
+
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
--- websocket/main.c
+++ websocket/main.c
@@ -20,62 +20,112 @@ void error(char *msg) {
     exit(1);
 }
 
-int sockfd;                     /* socket */
-char udp_buf[BUFSIZE];          /* message buf */
 int receive_flag = 0;
 char tmp[BUFSIZE];              /* tmp buf */
+pthread_t recv_thread;
 
 void * recv_data_thread(void *arg) {
-    int clientlen;                  /* byte size of client's address */
-    struct sockaddr_in clientaddr;  /* client addr */
-    struct hostent *hostp;          /* client host info */
-    char *hostaddrp;                /* dotted decimal host addr string */
-    int optval;                     /* flag value for setsockopt */
-    int n;                          /* message byte size */
+    int sockfd; /* socket */
+    int portno; /* port to listen on */
+    int clientlen; /* byte size of client's address */
+    struct sockaddr_in serveraddr; /* server's addr */
+    struct sockaddr_in clientaddr; /* client addr */
+    struct hostent *hostp; /* client host info */
+    char buf[BUFSIZE]; /* message buf */
+    char *hostaddrp; /* dotted decimal host addr string */
+    int optval; /* flag value for setsockopt */
+    int n; /* message byte size */
 
+    portno = atoi("9098");
+
+    /* 
+     * socket: create the parent socket 
+     */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    /* setsockopt: Handy debugging trick that lets 
+     * us rerun the server immediately after we kill it; 
+     * otherwise we have to wait about 20 secs. 
+     * Eliminates "ERROR on binding: Address already in use" error. 
+     */
+    optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
+            (const void *)&optval , sizeof(int));
+
+    /*
+     * build the server's Internet address
+     */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)portno);
+
+    /* 
+     * bind: associate the parent socket with a port 
+     */
+    if (bind(sockfd, (struct sockaddr *) &serveraddr, 
+                sizeof(serveraddr)) < 0) 
+        error("ERROR on binding");
+
+    /* 
+     * main loop: wait for a datagram, then echo it
+     */
+    clientlen = sizeof(clientaddr);
     while (1) {
 
         /*
          * recvfrom: receive a UDP datagram from a client
          */
-        bzero(udp_buf, BUFSIZE);
-        n = recvfrom(sockfd, udp_buf, BUFSIZE, 0,
-            (struct sockaddr *) &clientaddr, &clientlen);
-        /*
+        bzero(buf, BUFSIZE);
+        n = recvfrom(sockfd, buf, BUFSIZE, 0,
+                (struct sockaddr *) &clientaddr, &clientlen);
         if (n < 0)
             error("ERROR in recvfrom");
-        */
-	    if (strlen(udp_buf) == 8) {
+
+        printf("buf length: %d\r\n", strlen(buf));
+        if (strlen(buf) == 7 && strncmp(buf, "refresh", 7) == 0) {
+            printf("init tmp: %s.\n", tmp);
+        } else if (strlen(buf) == 8) {
+            bzero(tmp, BUFSIZE);
             strcpy(tmp, "{\"temperature\":");
-            strcpy(tmp + strlen("{\"temperature\":"), udp_buf + 6);
+            strcpy(tmp + strlen("{\"temperature\":"), buf + 6);
             tmp[strlen(tmp)] = '}';
             printf("STM32: %s\r\n", tmp);
-        } else {
-            strcpy(tmp, udp_buf);
+            receive_flag = 1;
+        } else if (strlen(buf) > 13){
+            bzero(tmp, BUFSIZE);
+            strcpy(tmp, buf);
             printf("Android: %s\r\n", tmp);
+            receive_flag = 1;
         }
 
         /* 
          * gethostbyaddr: determine who sent the datagram
          */
-        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        hostaddrp = inet_ntoa(clientaddr.sin_addr);
-        if (hostaddrp == NULL)
-            error("ERROR on inet_ntoa\n");
-        printf("server received datagram from %s\n", hostaddrp);
-        printf("server received %d/%d bytes: %s\n", (int)strlen(udp_buf), n, udp_buf);
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+                sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        //     hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        //     if (hostaddrp == NULL)
+        //       error("ERROR on inet_ntoa\n");
+        // printf("server received datagram from %s (%s)\n", 
+        // hostp->h_name, hostaddrp);
+        printf("server received %d/%d bytes: %s\n", strlen(buf), n, buf);
 
-        usleep(1000*1000);
-      
-        sendto(sockfd, "ok", strlen("ok"), 0, (struct sockaddr *) &clientaddr, clientlen);
-
-        receive_flag = 1;
+        /* 
+         * sendto: echo the input back to the client 
+         */
+        n = sendto(sockfd, tmp, strlen(tmp), 0, 
+                (struct sockaddr *) &clientaddr, clientlen);
+        if (n < 0) 
+            error("ERROR in sendto");
     }
 }
 
 static int callback_http(struct lws *wsi,
-                         enum lws_callback_reasons reason, void *user,
-                         void *in, size_t len)
+        enum lws_callback_reasons reason, void *user,
+        void *in, size_t len)
 {
     return 0;
 }
@@ -83,8 +133,8 @@ static int callback_http(struct lws *wsi,
 
 
 static int callback_dumb_increment(struct lws *wsi,
-                                   enum lws_callback_reasons reason,
-                                   void *user, void *in, size_t len)
+        enum lws_callback_reasons reason,
+        void *user, void *in, size_t len)
 {
 
     switch (reason) {
@@ -92,51 +142,66 @@ static int callback_dumb_increment(struct lws *wsi,
             printf("connection established\n");
             break;
         case LWS_CALLBACK_RECEIVE: { // the funny part
-            printf("received data: %s\n", (char *) in);
+                           printf("received data: %s\n", (char *) in);
 
-            int ret = 0;
-            int count = 0;
-            while(1) {
+                           int ret = 0;
+                           int count = 0;
 
-                if (receive_flag == 0 ) {
-                    if (count++ % 10 == 0) {
-                        char *ws_buf_clear = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING +
-                                                                     LWS_SEND_BUFFER_POST_PADDING);
-                        
-                        ret = lws_write(wsi, &ws_buf_clear[LWS_SEND_BUFFER_PRE_PADDING], 0, LWS_WRITE_TEXT);
-                        if (ret == -1) {
-                            break;
-                        }
+                           int back_len = strlen(tmp);
+                           char *ws_buf = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING + back_len +
+                                   LWS_SEND_BUFFER_POST_PADDING);
 
-                        free(ws_buf_clear);
-                    }
+                           strcpy(&ws_buf[LWS_SEND_BUFFER_PRE_PADDING], tmp);
 
-                    usleep(1000 * 100);
-                    continue;
-                }
+                           ret = lws_write(wsi, &ws_buf[LWS_SEND_BUFFER_PRE_PADDING], back_len, LWS_WRITE_TEXT);
+                           printf("ret = %d.\n", ret);
+                           if (ret == -1) {
+                               break;
+                           }
 
-                int back_len = strlen(tmp);
-                char *ws_buf = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING + back_len +
-                                                             LWS_SEND_BUFFER_POST_PADDING);
-                
-                strcpy(&ws_buf[LWS_SEND_BUFFER_PRE_PADDING], tmp);
-                        
-                ret = lws_write(wsi, &ws_buf[LWS_SEND_BUFFER_PRE_PADDING], back_len, LWS_WRITE_TEXT);
-                printf("ret = %d.\n", ret);
-                if (ret == -1) {
-                    break;
-                }
+                           free(ws_buf);
 
-                free(ws_buf);
+                           while(1) {
 
-                receive_flag = 0;
-            }
-            break;
-        }
+                               if (receive_flag == 0 ) {
+                                   if (count++ % 10 == 0) {
+                                       char *ws_buf_clear = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING +
+                                               LWS_SEND_BUFFER_POST_PADDING);
+
+                                       ret = lws_write(wsi, &ws_buf_clear[LWS_SEND_BUFFER_PRE_PADDING], 0, LWS_WRITE_TEXT);
+                                       if (ret == -1) {
+                                           break;
+                                       }
+
+                                       free(ws_buf_clear);
+                                   }
+
+                                   usleep(1000 * 100);
+                                   continue;
+                               }
+
+                               back_len = strlen(tmp);
+                               ws_buf = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING + back_len +
+                                       LWS_SEND_BUFFER_POST_PADDING);
+
+                               strcpy(&ws_buf[LWS_SEND_BUFFER_PRE_PADDING], tmp);
+
+                               ret = lws_write(wsi, &ws_buf[LWS_SEND_BUFFER_PRE_PADDING], back_len, LWS_WRITE_TEXT);
+                               printf("ret = %d.\n", ret);
+                               if (ret == -1) {
+                                   break;
+                               }
+
+                               free(ws_buf);
+
+                               receive_flag = 0;
+                           }
+                           break;
+                       }
         default:
-            break;
+                       break;
     }
-    
+
     return 0;
 }
 
@@ -178,61 +243,11 @@ int main(void) {
         return -1;
     }
 
-    
+
     printf("starting ws server...\n");
 
-
-    int portno; /* port to listen on */
-    int clientlen; /* byte size of client's address */
-    struct sockaddr_in serveraddr; /* server's addr */
-    struct sockaddr_in clientaddr; /* client addr */
-    struct hostent *hostp; /* client host info */
-    char udp_buf[BUFSIZE]; /* message buf */
-    char *hostaddrp; /* dotted decimal host addr string */
-    int optval; /* flag value for setsockopt */
-    int n; /* message byte size */
-    pthread_t recv_thread;
-
-    portno = 9098;
-
-    /* 
-     * socket: create the parent socket 
-     */
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) 
-      error("ERROR opening socket");
-
-    /* setsockopt: Handy debugging trick that lets 
-     * us rerun the server immediately after we kill it; 
-     * otherwise we have to wait about 20 secs. 
-     * Eliminates "ERROR on binding: Address already in use" error. 
-     */
-    optval = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-           (const void *)&optval , sizeof(int));
-
-    /*
-     * build the server's Internet address
-     */
-    bzero((char *) &serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons((unsigned short)portno);
-
-    /* 
-     * bind: associate the parent socket with a port 
-     */
-    if (bind(sockfd, (struct sockaddr *) &serveraddr, 
-         sizeof(serveraddr)) < 0) 
-      error("ERROR on binding");
-
-    /* 
-     * main loop: wait for a datagram, then echo it
-     */
-    clientlen = sizeof(clientaddr);
-
     pthread_create(&recv_thread, NULL, recv_data_thread, NULL);
-    
+
     // infinite loop, to end this server send SIGTERM. (CTRL+C)
     while (1) {
         lws_service(context, 50);
@@ -241,8 +256,8 @@ int main(void) {
         // (this is a single threaded webserver and this will keep our server
         // from generating load while there are not requests to process)
     }
-    
+
     lws_context_destroy(context);
-    
+
     return 0;
 }
